503cb8469819ce6bda354ee6d2c2345007f5cca8
Marco FORDELONE <72397460+LeThorgrim@users.noreply.github.com>
2025-02-12 21:48:48 +0100

    half done

--- src/main/java/fr/isen/java2/db/daos/GenreDao.java
+++ src/main/java/fr/isen/java2/db/daos/GenreDao.java
@@ -1,20 +1,74 @@
 package fr.isen.java2.db.daos;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import fr.isen.java2.db.entities.Genre;
 
+import static fr.isen.java2.db.daos.DataSourceFactory.getDataSource;
+
 public class GenreDao {
 
 	public List<Genre> listGenres() {
-		throw new RuntimeException("Method is not yet implemented");
+		List<Genre> genres = new ArrayList<>();
+		String cmdSQL = "SELECT * FROM genre";
+
+		// less code than in class w/ triple try in one
+		try (Connection connection = getDataSource().getConnection();
+			 Statement statement = connection.createStatement();
+			 ResultSet resultSet = statement.executeQuery(cmdSQL)) {
+
+			while (resultSet.next()) {
+				Genre genre = new Genre();
+				genre.setId(resultSet.getInt("idgenre"));
+				genre.setName(resultSet.getString("name"));
+				genres.add(genre);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return genres;
 	}
 
 	public Genre getGenre(String name) {
-		throw new RuntimeException("Method is not yet implemented");
+		Genre genre = null;
+		String cmdSQL = "SELECT * FROM genre WHERE name = ?";
+
+		// less code than in class w/ double try in one
+		try (Connection connection = getDataSource().getConnection();
+			 PreparedStatement statement = connection.prepareStatement(cmdSQL)) {
+
+			statement.setString(1, name);
+			ResultSet resultSet = statement.executeQuery();
+
+			if (resultSet.next()) {
+				genre = new Genre();
+				genre.setId(resultSet.getInt("idgenre"));
+				genre.setName(resultSet.getString("name"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return genre;
 	}
 
 	public void addGenre(String name) {
-		throw new RuntimeException("Method is not yet implemented");
+		String cmdSQL = "INSERT INTO genre(name) VALUES(?)";
+
+		// less code than in class w/ double try in one
+		try (Connection connection = getDataSource().getConnection();
+			 PreparedStatement statement = connection.prepareStatement(cmdSQL)) {
+
+			statement.setString(1, name);
+			statement.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
